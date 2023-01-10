89626fd8e7eac4825ffa5ac8662287ec08661fed
qian1170 <maomaomaovo@gmail.com>
2023-01-10 05:04:44 +0100

    '4full'

--- store/utils.py
+++ store/utils.py
@@ -56,3 +56,31 @@ def cartData(request):
         order = cookieData['order']
         items = cookieData['items']
     return {'cartItems':cartItems,'order':order,'items':items}
+
+def guestOrder(request,data):
+    name = data['form']['name']
+    email = data['form']['email']
+
+    cookieData = cookieCart(request)
+    items=cookieData['items']
+
+    customer,created = Customer.objects.get_or_create(
+        email=email,
+    )
+    customer.name = name
+    customer.save()
+
+    order=Order.objects.create(
+        customer=customer,
+        complete=False,
+    )
+
+    for item in items:
+        product=Product.objects.get(id=item['id'])
+        orderItem = OrderItem.objects.create(
+            product=product,
+            order=order,
+            quantity=item['quantity'],
+        )
+    return customer,order
+    
--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from .models import *
 from django.http import JsonResponse
 import json 
 import datetime
-from .utils import cookieCart,cartData
+from .utils import cookieCart,cartData,guestOrder
 
 
 
@@ -73,23 +73,25 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer=customer,complete=False)
+
+    else:
+        customer,order=guestOrder(request,data)
+
         total=float(data['form']['total'])
         order.transaction_id = transaction_id
 
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
 
-        if order.shipping ==True:
-            Shippingaddress.objects.create(
-                customer = customer,
-                order = order,
-                address = data['shipping']['address'],
-                city = data['shipping']['city'],
-                state = data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
-
-    else:
-        print('user is not logged in')
+    if order.shipping ==True:
+        ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+    
     return JsonResponse('Payment submitted',safe=Flase)
