442b7b8069e8ad6e1969894a44e715e4b586e659
wuqian <wuqian.ma@efrei.net>
2023-12-10 10:10:38 +0100

    1

--- store/tests/e2e/test_e2e.py
+++ store/tests/e2e/test_e2e.py
@@ -12,7 +12,7 @@ class MyE2ETest(StaticLiveServerTestCase):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-dev-shm-usage')
-        cls.selenium = webdriver.Chrome(options=options)
+        cls.selenium = webdriver.Chrome()
 
 
         cls.selenium.implicitly_wait(10)
