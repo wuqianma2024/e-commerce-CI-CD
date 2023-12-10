023352b47711c45cd379b039ea04303cf22c2b4a
wuqian <wuqian.ma@efrei.net>
2023-12-10 10:08:23 +0100

    update e2etest

--- store/tests/e2e/test_e2e.py
+++ store/tests/e2e/test_e2e.py
@@ -11,6 +11,7 @@ class MyE2ETest(StaticLiveServerTestCase):
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-dev-shm-usage')
         cls.selenium = webdriver.Chrome(options=options)
 
 
